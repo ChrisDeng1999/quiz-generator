d3fd5895c9e62764afd33ade9723c9e5ee7aecf6
sagegod <cdeng1999@gmail.com>
2022-06-04 18:08:58 -0700

    It now shows right and wrong answers as well as loops through all the questions in the array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -112,7 +112,19 @@ function selectAnswer (event) {
     }
 }
 
+function setStatusClass (element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
 
+function clearStatusClass (element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
 
 startButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
